81f06334d6047c44065997767d1e30a52d562b5a
tsheporamantso <tshepo.ramantso@outlook.com>
2023-03-27 21:53:48 +0200

    Add style.css loader and add background color

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-// import _ from 'lodash';
+import _ from 'lodash';
+import './style.css';
 
-// function component () {
-//     const element = document.createElement('div');
+function component () {
+    const element = document.createElement('div');
 
-//     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-//     return element;
-// }
+    return element;
+}
 
-// document.body.appendChild(component());
+document.body.appendChild(component());
