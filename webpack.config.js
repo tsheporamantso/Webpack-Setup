462e25184f050e6fd086975f6a7a216f2fc33570
tsheporamantso <tshepo.ramantso@outlook.com>
2023-03-28 07:50:28 +0200

    Add webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,21 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    mode:'development',
     entry: './src/index.js',
     output: {
-        filename: 'main.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
     },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
     plugins: [
         new HtmlWebpackPlugin({
             template: './src/index.html',
+            title: 'Development',
         }),
     ],
     module: {
@@ -21,4 +27,7 @@ module.exports = {
             },
         ],
     },
+    optimization: {
+        runtimeChunk: 'single',
+      },
 };
